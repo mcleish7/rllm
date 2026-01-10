ed5d29cbafbc2e7aaf7e78bc4b479b2b703df603
mcleish7 <smcleish7@hotmail.co.uk>
2026-01-10 13:53:16 +0100

    trying to run baseline

--- examples/deepscaler/7_jan/run.sh
+++ examples/deepscaler/7_jan/run.sh
@@ -47,3 +47,24 @@ python /capstor/scratch/cscs/smcleish/llnl-tools/launch_daint.py \
     --pass_run_name=False \
     --custom_invocation='DS_NAME="deepscaler_math_error_at_k_saved_start_0_end_20000_num_completions_10_random" bash examples/deepscaler/7_jan/dataset_env_var_launch.sh'
 
+python /capstor/scratch/cscs/smcleish/llnl-tools/launch_daint.py \
+    --output_dir=/capstor/scratch/cscs/smcleish/rllm_daint_291_129_uenv_venv_rllm/tuo_outputs \
+    --run_name=deepscaler-1.5b-8k-reproduce-first-run \
+    --nodes=2 \
+    --gpus_per_node=4 \
+    --ntasks_per_node=1 \
+    --cpus_per_task=72 \
+    --minutes=1440 \
+    --pass_run_name=False \
+    --custom_invocation='DS_NAME="deepscaler_math" bash examples/deepscaler/7_jan/dataset_env_var_launch.sh'
+
+python /capstor/scratch/cscs/smcleish/llnl-tools/launch_daint.py \
+    --output_dir=/capstor/scratch/cscs/smcleish/rllm_daint_291_129_uenv_venv_rllm/tuo_outputs \
+    --run_name=deepscaler-1.5b-8k-reproduce-first-run-with-shuffle \
+    --nodes=2 \
+    --gpus_per_node=4 \
+    --ntasks_per_node=1 \
+    --cpus_per_task=72 \
+    --minutes=1440 \
+    --pass_run_name=False \
+    --custom_invocation='DS_NAME="deepscaler_math" bash examples/deepscaler/7_jan/dataset_env_var_launch_with_shuffle.sh'
